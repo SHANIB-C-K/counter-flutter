19d171a9f0b2768919599bb56388e339e3194e20
SHANIB C K <ckshanib8@gmail.com>
2024-05-17 18:06:43 +0530

    ui design completed

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -6,6 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: const Text(
           'CounterApp',
@@ -15,7 +16,135 @@ class HomeScreen extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.green,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 208, 206, 206),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 33, 33, 33),
+                    blurRadius: 4,
+                    offset: Offset(0, 8), // Shadow position
+                  ),
+                ],
+              ),
+              child: const Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Counter',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    '0',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: const MaterialStatePropertyAll(
+                      Colors.orange,
+                    ),
+                    shape: MaterialStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    fixedSize: const MaterialStatePropertyAll(
+                      Size(110, 50),
+                    ),
+                    shadowColor: const MaterialStatePropertyAll(Colors.black),
+                  ),
+                  child: const Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: const MaterialStatePropertyAll(
+                      Colors.red,
+                    ),
+                    shape: MaterialStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    shadowColor: const MaterialStatePropertyAll(
+                      Colors.black,
+                    ),
+                    fixedSize: const MaterialStatePropertyAll(
+                      Size(110, 50),
+                    ),
+                  ),
+                  child: const Text(
+                    '-',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: const MaterialStatePropertyAll(
+                      Colors.green,
+                    ),
+                    shape: MaterialStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    shadowColor: const MaterialStatePropertyAll(
+                      Colors.black,
+                    ),
+                    fixedSize: const MaterialStatePropertyAll(
+                      Size(110, 50),
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
